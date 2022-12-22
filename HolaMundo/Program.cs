470e3a8189f21b8e510fd492cb73c042c6646cdb
Yovanny Rodriguez <yovannyr87@gmail.com>
2022-12-22 11:39:14 -0400

    Completada

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+string nombre = "Yovanny Rodriguez";
+Console.WriteLine("Mi nombre es: " + nombre);
+
+
